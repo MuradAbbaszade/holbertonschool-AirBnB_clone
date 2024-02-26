7a4ce5ad2a6e16fd752570e292779e15f12c4eb7
Murad Abbaszade <muradabbaszade6@gmail.com>
2024-02-26 12:27:43 +0000

    Update init function

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,19 @@ from datetime import datetime
 
 class BaseModel:
     """Base Model class"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Init function"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for i in kwargs.keys():
+                if i == "created_at" or i == "updated_at":
+                    date = datetime.fromisoformat(kwargs[i])
+                    setattr(self, i, date)
+                elif i != "__class__":
+                    setattr(self, i, kwargs[i])
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Str function"""
