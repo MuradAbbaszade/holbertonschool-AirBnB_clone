6b5d0aaacbba7691a952ba9e665950d53a2ebbe9
fuartkhana <146414387+fuartkhana@users.noreply.github.com>
2024-03-03 03:14:39 +0400

    Working on unittest

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -35,18 +35,23 @@ class TestUser(TestCase):
         self.assertEqual(new["updated_at"], self.inst.updated_at.isoformat())
         self.assertEqual(new['__class__'], self.inst.__class__.__name__)
 
+    def test__str__(self):
+        className = self.inst.__class__.__name__
+        result = "[{}] ({}) {}".format(className, self.inst.id, self.inst.__dict__)
+        self.assertEqual(result, self.inst.__str__())
 
+    def test_email(self):
+        self.assertTrue(hasattr(self.user, "email"))
+        self.assertEqual(self.user.email, "")
 
+    def test_password(self):
+        self.assertTrue(hasattr(self.user, "password"))
+        self.assertEqual(self.user.password, "")
 
+    def test_first_name(self):
+        self.assertTrue(hasattr(self.user, "first_name"))
+        self.assertEqual(self.user.first_name, "")
 
-
-
-"""    from models.base_model import BaseModel
-
-    class User(BaseModel):
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
-
-        """
+    def test_last_name(self):
+        self.assertTrue(hasattr(self.user, "last_name"))
+        self.assertEqual(self.user.last_name, "")
