408af5f4543b39a80e3113d78ac0165dafbcb290
fuartkhana <146414387+fuartkhana@users.noreply.github.com>
2024-03-03 04:01:41 +0400

    ADDED unittest for State

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Unittest for State Class attributes and methods
+"""
+from unittest import TestCase
+from models.state import State
+from datetime import datetime
+
+
+class TestState(TestCase):
+    """Defines variables and methods"""
+    def setUp(self):
+        """
+        Sets up the public attributes and methods for the TestUser class
+        """
+        self.inst1 = State()
+        self.inst1.created_at = datetime.now()
+        self.inst1.updated_at = datetime.now()
+        self.inst2 = State()
+
+    def test_id(self):
+        self.model_test = State()
+        self.assertNotEqual(self.inst1.id, self.model_test.id)
+
+    def test_save(self):
+        updated_at = self.inst1.updated_at
+        self.inst1.save()
+        self.assertNotEqual(updated_at, self.inst1.updated_at)
+        with open("file.json", "r") as file:
+            self.assertIn("State.{}".format(self.inst1.id), file.read())
+
+    def test_to_dict(self):
+        new = self.b1.to_dict()
+        self.assertEqual(new["created_at"], self.inst1.created_at.isoformat())
+        self.assertEqual(new["updated_at"], self.inst1.updated_at.isoformat())
+        self.assertEqual(new['__class__'], self.inst1.__class__.__name__)
+
+    def test__str__(self):
+        className = self.inst1.__class__.__name__
+        result = "[{}] ({}) {}".format(className, self.inst1.id, self.inst1.__dict__)
+        self.assertEqual(result, self.inst1.__str__())
+
+    def test_name(self):
+        self.assertTrue(hasattr(self.inst2, "name"))
+        self.assertEqual(self.inst2.name, "")
