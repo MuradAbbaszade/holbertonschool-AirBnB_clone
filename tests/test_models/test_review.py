e526b95f73568acdb8abe145a94a118684ca22a2
fuartkhana <146414387+fuartkhana@users.noreply.github.com>
2024-03-03 04:14:18 +0400

    ADDED unittest for reveiw

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+Unittest for Review Class attributes and methods
+"""
+from unittest import TestCase
+from models.review import Review
+from datetime import datetime
+
+
+class TestReview(TestCase):
+    """Defines variables and methods"""
+    def setUp(self):
+        """
+        Sets up the public attributes and methods for the TestUser class
+        """
+        self.inst1 = Review()
+        self.inst1.created_at = datetime.now()
+        self.inst1.updated_at = datetime.now()
+        self.inst2 = Review()
+
+    def test_id(self):
+        self.model_test = Review()
+        self.assertNotEqual(self.inst1.id, self.model_test.id)
+
+    def test_save(self):
+        updated_at = self.inst1.updated_at
+        self.inst1.save()
+        self.assertNotEqual(updated_at, self.inst1.updated_at)
+        with open("file.json", "r") as file:
+            self.assertIn("Review.{}".format(self.inst1.id), file.read())
+
+    def test_to_dict(self):
+        new = self.inst1.to_dict()
+        self.assertEqual(new["created_at"], self.inst1.created_at.isoformat())
+        self.assertEqual(new["updated_at"], self.inst1.updated_at.isoformat())
+        self.assertEqual(new['__class__'], self.inst1.__class__.__name__)
+
+    def test__str__(self):
+        className = self.inst1.__class__.__name__
+        result = "[{}] ({}) {}".format(className, self.inst1.id, self.inst1.__dict__)
+        self.assertEqual(result, self.inst1.__str__())
+
+    def test_place_id(self):
+        self.assertTrue(hasattr(self.inst2, "place_id"))
+        self.assertEqual(self.inst2.place_id, "")
+
+    def test_user_id(self):
+        self.assertTrue(hasattr(self.inst2, "user_id"))
+        self.assertEqual(self.inst2.user_id, "")
+
+    def test_text(self):
+        self.assertTrue(hasattr(self.b2, "text"))
+        self.assertEqual(self.b2.text, "")
